59c0cb69019e9b883b4105987fba0587086e8394
kdutkiewicz <kdutkiewicz@inpost.pl>
2024-01-20 11:47:21 +0100

    init spring boot

---
+++ src/main/java/com/firestms/FleetManager.java
@@ -0,0 +1,12 @@
+package com.firestms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetManager.class, args);
+    }
+}
