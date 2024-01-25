22b2aacda64c6973c646fe4975e7aece37f49e1e
kdutkiewicz <kdutkiewicz@inpost.pl>
2024-01-25 18:18:41 +0100

    refactor assignment test

--- src/test/java/com/firetms/AssignmentServiceTest.java
+++ src/test/java/com/firetms/AssignmentServiceTest.java
@@ -10,6 +10,7 @@ import com.firestms.repository.TrailerRepository;
 import com.firestms.service.AssignmentService;
 import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -29,6 +30,11 @@ import java.util.List;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ContextConfiguration(classes = TestWebConfig.class)
 public class AssignmentServiceTest {
+    
+    private final String CAR1= "CAR1";
+    private final String CAR2 = "CAR2";
+    private final String TRAILER1 = "TRAILER1";
+    private final String TRAILER2 = "TRAILER2";
 
     @Autowired
     private CarRepository carRepository;
@@ -47,69 +53,51 @@ public class AssignmentServiceTest {
         assignmentRepository.deleteAll();
     }
 
+    @BeforeAll
+    public void setUpAll(){
+        carRepository.save(new Car(CAR1));
+        carRepository.save(new Car(CAR2));
+        trailerRepository.save(new Trailer(TRAILER1));
+        trailerRepository.save(new Trailer(TRAILER2));
+    }
 
     @Test
     public void successfullyAddAssignments() {
-        addCar("CAR1234");
-        addTrailer("TR1234");
-        assignmentService.addNewAssignment(getAssignment("CAR1234", "TR1234", Instant.parse("2024-01-05T00:00:00Z"), Instant.parse("2024-01-08T00:00:00Z")));
-        assignmentService.addNewAssignment(getAssignment("CAR1234", "TR1234", Instant.parse("2024-01-09T00:00:00Z"), Instant.parse("2024-01-12T00:00:00Z")));
-        Assertions.assertEquals(assignmentRepository.findAllByCarRegistrationNumber("CAR1234").size(), 2);
-
-
+        assignmentService.addNewAssignment(getAssignment(CAR1, TRAILER1, Instant.parse("2024-01-05T00:00:00Z"), Instant.parse("2024-01-08T00:00:00Z")));
+        assignmentService.addNewAssignment(getAssignment(CAR1, TRAILER1, Instant.parse("2024-01-09T00:00:00Z"), Instant.parse("2024-01-12T00:00:00Z")));
+        Assertions.assertEquals(assignmentRepository.findAllByCarRegistrationNumber(CAR1).size(), 2);
     }
 
     @Test
     public void addingAssignmentWhenThereIsAlreadyAssignmentForCarShouldThrowAnException() {
-        addCar("CAR123");
-        addTrailer("TR123");
-        var carRegistrationNumber = "CAR123";
-        var trailerRegistrationNumber = "TR123";
-        var startTime1 = "2024-01-01T00:00:00Z";
-        var endTime1 = "2024-01-02T00:00:00Z";
-        var startTime2 = "2024-01-05T00:00:00Z";
-        var endTime2 = "2024-01-09T00:00:00Z";
-        assignmentRepository.save(
-            Assignment.builder()
-                .carRegistrationNumber(carRegistrationNumber)
-                .trailerRegistrationNumber(trailerRegistrationNumber)
-                .startTime(Instant.parse(startTime1))
-                .endTime(Instant.parse(endTime1)).build()
-        );
-        assignmentRepository.save(
-            Assignment.builder()
-                .carRegistrationNumber(carRegistrationNumber)
-                .trailerRegistrationNumber(trailerRegistrationNumber)
-                .startTime(Instant.parse(startTime2))
-                .endTime(Instant.parse(endTime2)).build()
-        );
+        var startTime1 = Instant.parse("2024-01-01T00:00:00Z");
+        var endTime1 = Instant.parse("2024-01-02T00:00:00Z");
+        var startTime2 = Instant.parse("2024-01-05T00:00:00Z");
+        var endTime2 = Instant.parse("2024-01-09T00:00:00Z");
+        
+        addAssignmentDirectlyToRepo(CAR1, TRAILER1, startTime1, endTime1);
+        addAssignmentDirectlyToRepo(CAR1, TRAILER1, startTime2, endTime2);
+        
         Exception exception = Assertions.assertThrows(AssignmentException.class, () ->
-            assignmentService.addNewAssignment(getAssignment("CAR123", "TR123", Instant.parse("2024-01-03T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"))));
-        String expectedMessage = "The car with registration number:CAR123 already has an assignment in this period of time";
+            assignmentService.addNewAssignment(getAssignment(CAR1, TRAILER1, Instant.parse("2024-01-03T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"))));
+        String expectedMessage = "The car with registration number:"+CAR1+" already has an assignment in this period of time";
 
         Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     public void addingAssignmentWhenThereIsAlreadyAssignmentForTrailerShouldThrowAnException() {
-        addCar("CAR11");
-        addCar("CAR12");
-        addTrailer("TR11");
-        addAssignmentDirectlyToRepo("CAR11", "TR11", Instant.parse("2024-01-01T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"));
+        addAssignmentDirectlyToRepo(CAR1, TRAILER1, Instant.parse("2024-01-01T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"));
         List<Assignment> asd = Lists.newArrayList(assignmentRepository.findAll());
         Exception exception = Assertions.assertThrows(AssignmentException.class, () ->
-            assignmentService.addNewAssignment(getAssignment("CAR12", "TR11", Instant.parse("2024-01-03T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"))));
-        String expectedMessage = "The trailer with registration number:TR11 already has an assignment in this period of time";
+            assignmentService.addNewAssignment(getAssignment(CAR2, TRAILER1, Instant.parse("2024-01-03T00:00:00Z"), Instant.parse("2024-01-06T00:00:00Z"))));
+        String expectedMessage = "The trailer with registration number:"+TRAILER1+" already has an assignment in this period of time";
 
         Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
-    void addCar(String registrationNumber) {
-        carRepository.save(new Car(registrationNumber));
-    }
-
-    void addTrailer(String registrationNumber) {
-        trailerRepository.save(new Trailer(registrationNumber));
+    @Test
+    public void overrideAssignmentWhenNewAssignmentTakeAllTimeOfOldOne() {
     }
 
 
